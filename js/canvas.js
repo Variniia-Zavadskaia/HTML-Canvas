e90c1daa8657b923d0d1069b8a2aff0ccd67616b
Variniia Zavadskaia <variniyaal@gmail.com>
2024-06-10 17:13:47 +0300

    befor pencil

--- js/canvas.js
+++ js/canvas.js
@@ -2,42 +2,47 @@
 var gElCanvas
 var gCtx
 var gCurrShape
+var gBckg
+var gShpColor
+var isDrawing
 
 function onInit() {
     gElCanvas = document.querySelector('canvas')
     gCtx = gElCanvas.getContext('2d')
+    isDrawing = false;
     console.log('gCtx:', gCtx)
+    onSetShpColor()
     onSetShape();
     resizeCanvas()
     window.addEventListener('resize', resizeCanvas)
 }
 
-function drawArc(x, y) {
-    gCtx.beginPath()
-    gCtx.lineWidth = 3
+// function drawArc(x, y) {
+//     gCtx.beginPath()
+//     gCtx.lineWidth = 3
 
-    //* The x,y cords of the center , The radius, The starting angle, The ending angle, in radians
-    // gCtx.arc(x, y, 70, 0, Math.PI) //* draws a circle
-    gCtx.arc(x, y, 70, 0, Math.PI * 2) //* draws a circle
-    // gCtx.fillStyle = ' '
-    // gCtx.fill()
-    gCtx.strokeStyle = 'black'
-    gCtx.stroke()
-}
+//     //* The x,y cords of the center , The radius, The starting angle, The ending angle, in radians
+//     // gCtx.arc(x, y, 70, 0, Math.PI) //* draws a circle
+//     gCtx.arc(x, y, 70, 0, Math.PI * 2) //* draws a circle
+//     // gCtx.fillStyle = ' '
+//     // gCtx.fill()
+//     gCtx.strokeStyle = gShpColor
+//     gCtx.stroke()
+// }
 
-function drawRect(x, y) {
-    gCtx.beginPath()
-    gCtx.strokeStyle = 'purple'
-    // gCtx.fillStyle = 'royalblue'
+// function drawRect(x, y) {
+//     gCtx.beginPath()
+//     gCtx.strokeStyle = gShpColor
+//     // gCtx.fillStyle = 'black'
 
-    gCtx.lineWidth = 3
-    gCtx.rect(x, y, 120, 120)
-    // gCtx.fill()
-    gCtx.stroke()
-    //* THE SAME
-    // gCtx.fillRect(x, y, 120, 120)
-    // gCtx.strokeRect(x, y, 120, 120)
-}
+//     gCtx.lineWidth = 3
+//     gCtx.rect(x, y, 120, 120)
+//     // gCtx.fill()
+//     gCtx.stroke()
+//     //* THE SAME
+//     // gCtx.fillRect(x, y, 120, 120)
+//     // gCtx.strokeRect(x, y, 120, 120)
+// }
 
 function onClearCanvas() {
     gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height)
@@ -52,6 +57,18 @@ function resizeCanvas() {
     // drawText('Drawing text!', gElCanvas.width / 2, gElCanvas.height / 2)
 }
 
+function onSetBckGrnd() {
+    const elClrChoice = document.getElementById('bcg')
+    gBckg = elClrChoice.value
+    const elCanvas = document.querySelector('canvas')
+    elCanvas.style.backgroundColor = gBckg
+}
+
+function onSetShpColor(){
+    const elClrChoice = document.getElementById('shpColor')
+    gShpColor = elClrChoice.value
+}
+
 function onSetShape() {
     // console.log('ghj');
     const elShapeChoice = document.querySelector('.shape-choice')
@@ -59,23 +76,46 @@ function onSetShape() {
     // elShapeChoice.innerText = capitalize(gCurrShape)
 }
 
-function onDraw(ev) {
-    // const offsetX = ev.offsetX
-    // const offsetY = ev.offsetY
-    const { offsetX, offsetY } = ev
-    // console.log('offsetX, offsetY:', offsetX, offsetY)
+function drawShape(x, y) {
+    gCtx.beginPath()
+    gCtx.strokeStyle = gShpColor
+    // gCtx.fillStyle = 'black'
+
+    gCtx.lineWidth = 3
 
     switch (gCurrShape) {
         // case 'line':
         //     drawLine(offsetX, offsetY)
         //     break
         case 'circle':
-            drawArc(offsetX, offsetY)
+            gCtx.arc(x, y, 70, 0, Math.PI * 2) //* draws a circle
             break
         case 'square':
-            drawRect(offsetX, offsetY)
+            gCtx.rect(x, y, 120, 120)
             break
     }
+
+    gCtx.stroke()
+}
+
+function onDraw(ev) {
+    // const offsetX = ev.offsetX
+    // const offsetY = ev.offsetY
+    const { offsetX, offsetY } = ev
+    // console.log('offsetX, offsetY:', offsetX, offsetY)
+    
+    drawShape(offsetX, offsetY)
+    // switch (gCurrShape) {
+    //     // case 'line':
+    //     //     drawLine(offsetX, offsetY)
+    //     //     break
+    //     case 'circle':
+    //         drawArc(offsetX, offsetY)
+    //         break
+    //     case 'square':
+    //         drawRect(offsetX, offsetY)
+    //         break
+    // }
 }
 
 function getCanvasCenter() {
@@ -88,3 +128,24 @@ function getCanvasCenter() {
 function capitalize(txt) {
     return txt.charAt(0).toUpperCase() + txt.substring(1)
 }
+
+// const canvas = document.getElementById('drawCanvas');
+// const ctx = canvas.getContext('2d');
+// let isDrawing = false;
+
+canvas.onmousedown = ({offsetX, offsetY}) => {
+    isDrawing = true;
+    gCtx.beginPath();
+    gCtx.moveTo(offsetX, offsetY);
+};
+
+canvas.onmousemove = ({offsetX, offsetY}) => {
+    if (isDrawing) {
+        gCtx.lineTo(offsetX, offsetY);
+        gCtx.stroke();
+    }
+};
+
+canvas.onmouseup = () => {
+    isDrawing = false;
+};
