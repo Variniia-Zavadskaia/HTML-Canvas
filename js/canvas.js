ed41d4c436d424e7310ba241f9242fc4c734a5f8
Variniia Zavadskaia <variniyaal@gmail.com>
2024-06-10 19:56:35 +0300

    finish mouse work

--- js/canvas.js
+++ js/canvas.js
@@ -5,56 +5,30 @@ var gCurrShape
 var gBckg
 var gShpColor
 var isDrawing
+var gPrevPos
+var gCanvas
+const TOUCH_EVS = ['touchstart', 'touchmove', 'touchend']
 
 function onInit() {
     gElCanvas = document.querySelector('canvas')
     gCtx = gElCanvas.getContext('2d')
     isDrawing = false;
-    console.log('gCtx:', gCtx)
+    addListeners()
+
     onSetShpColor()
     onSetShape();
     resizeCanvas()
-    window.addEventListener('resize', resizeCanvas)
+    // window.addEventListener('resize', resizeCanvas)
 }
 
-// function drawArc(x, y) {
-//     gCtx.beginPath()
-//     gCtx.lineWidth = 3
-
-//     //* The x,y cords of the center , The radius, The starting angle, The ending angle, in radians
-//     // gCtx.arc(x, y, 70, 0, Math.PI) //* draws a circle
-//     gCtx.arc(x, y, 70, 0, Math.PI * 2) //* draws a circle
-//     // gCtx.fillStyle = ' '
-//     // gCtx.fill()
-//     gCtx.strokeStyle = gShpColor
-//     gCtx.stroke()
-// }
-
-// function drawRect(x, y) {
-//     gCtx.beginPath()
-//     gCtx.strokeStyle = gShpColor
-//     // gCtx.fillStyle = 'black'
-
-//     gCtx.lineWidth = 3
-//     gCtx.rect(x, y, 120, 120)
-//     // gCtx.fill()
-//     gCtx.stroke()
-//     //* THE SAME
-//     // gCtx.fillRect(x, y, 120, 120)
-//     // gCtx.strokeRect(x, y, 120, 120)
-// }
 
 function onClearCanvas() {
     gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height)
-    //* We may clear part of the canvas
-    // gCtx.clearRect(0, 0, gElCanvas.width / 2, gElCanvas.height / 2)
 }
 
 function resizeCanvas() {
     const elContainer = document.querySelector('.canvas-container')
-    //* Changing the canvas dimension clears the canvas
-    gElCanvas.width = elContainer.clientWidth - 40  //* Subtracting 20px padding from each side
-    // drawText('Drawing text!', gElCanvas.width / 2, gElCanvas.height / 2)
+    gElCanvas.width = elContainer.clientWidth - 40   
 }
 
 function onSetBckGrnd() {
@@ -70,28 +44,32 @@ function onSetShpColor(){
 }
 
 function onSetShape() {
-    // console.log('ghj');
     const elShapeChoice = document.querySelector('.shape-choice')
     gCurrShape = elShapeChoice.value
-    // elShapeChoice.innerText = capitalize(gCurrShape)
 }
 
 function drawShape(x, y) {
     gCtx.beginPath()
     gCtx.strokeStyle = gShpColor
-    // gCtx.fillStyle = 'black'
 
-    gCtx.lineWidth = 3
+    gCtx.lineWidth = 1
 
     switch (gCurrShape) {
-        // case 'line':
-        //     drawLine(offsetX, offsetY)
-        //     break
+        case 'pencil':
+            gCtx.lineWidth = 1
+            gCtx.moveTo(gPrevPos.x, gPrevPos.y)
+            gCtx.lineTo(x,y)
+            // gCtx.rect(x, y, 1, 1)
+            break
         case 'circle':
-            gCtx.arc(x, y, 70, 0, Math.PI * 2) //* draws a circle
+            var dist = Math.sqrt( Math.abs(gPrevPos.x - x)**2 + Math.abs(gPrevPos.y - y)**2)
+
+            gCtx.arc(x, y, dist/2, 0, Math.PI * 2) //* draws a circle
             break
         case 'square':
-            gCtx.rect(x, y, 120, 120)
+            var dist = Math.sqrt( Math.abs(gPrevPos.x - x)**2 + Math.abs(gPrevPos.y - y)**2)
+            // gCtx.rect(x, y, dist, dist)
+            gCtx.rect(x, y, dist, 5*dist)
             break
     }
 
@@ -99,23 +77,8 @@ function drawShape(x, y) {
 }
 
 function onDraw(ev) {
-    // const offsetX = ev.offsetX
-    // const offsetY = ev.offsetY
     const { offsetX, offsetY } = ev
-    // console.log('offsetX, offsetY:', offsetX, offsetY)
-    
     drawShape(offsetX, offsetY)
-    // switch (gCurrShape) {
-    //     // case 'line':
-    //     //     drawLine(offsetX, offsetY)
-    //     //     break
-    //     case 'circle':
-    //         drawArc(offsetX, offsetY)
-    //         break
-    //     case 'square':
-    //         drawRect(offsetX, offsetY)
-    //         break
-    // }
 }
 
 function getCanvasCenter() {
@@ -125,27 +88,84 @@ function getCanvasCenter() {
     }
 }
 
-function capitalize(txt) {
-    return txt.charAt(0).toUpperCase() + txt.substring(1)
-}
 
-// const canvas = document.getElementById('drawCanvas');
-// const ctx = canvas.getContext('2d');
-// let isDrawing = false;
-
-canvas.onmousedown = ({offsetX, offsetY}) => {
+function onDown(ev) {
+    const pos = getEvPos(ev)
     isDrawing = true;
-    gCtx.beginPath();
-    gCtx.moveTo(offsetX, offsetY);
+    gPrevPos = {x: pos.x, y: pos.y}
+    
+    // drawShape(pos.x, pos.y)
 };
 
-canvas.onmousemove = ({offsetX, offsetY}) => {
+function onMove(ev) {
     if (isDrawing) {
-        gCtx.lineTo(offsetX, offsetY);
-        gCtx.stroke();
+        const pos = getEvPos(ev)
+
+        drawShape(pos.x, pos.y)
+        gPrevPos = {x: pos.x, y: pos.y}
+
     }
 };
 
-canvas.onmouseup = () => {
+function onUp() {
     isDrawing = false;
 };
+
+function addListeners() {
+    addMouseListeners()
+    // addTouchListeners()
+    //* Listen for resize ev
+    window.addEventListener('resize', () => {
+        resizeCanvas()
+        //* Calc the center of the canvas
+        const center = { x: gElCanvas.width / 2, y: gElCanvas.height / 2 }
+        //* Create the circle in the center
+        // createCircle(center)
+        // renderCanvas()
+    })
+}
+
+function addMouseListeners() {
+    gElCanvas.addEventListener('mousedown', onDown)
+    gElCanvas.addEventListener('mousemove', onMove)
+    gElCanvas.addEventListener('mouseup', onUp)
+}
+
+function addTouchListeners() {
+    gElCanvas.addEventListener('touchstart', onDown)
+    gElCanvas.addEventListener('touchmove', onMove)
+    gElCanvas.addEventListener('touchend', onUp)
+}
+
+function getEvPos(ev) {
+
+    let pos = {
+        x: ev.offsetX,
+        y: ev.offsetY,
+    }
+
+    // if (TOUCH_EVS.includes(ev.type)) {
+    //     //* Prevent triggering the mouse ev
+    //     ev.preventDefault()
+    //     //* Gets the first touch point
+    //     ev = ev.changedTouches[0]
+    //     //* Calc the right pos according to the touch screen
+    //     pos = {
+    //         x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+    //         y: ev.pageY - ev.target.offsetTop - ev.target.clientTop,
+    //     }
+    //     // console.log('pos:', pos)
+    // }
+    return pos
+}
+
+// download ////////////////////////////////////////////////
+
+// function onSelectImg(elImg) {
+//     gCtx.drawImage(elImg, 0, 0, gCanvas.width, gCanvas.height)
+// }
+
+function onDownloadCanvas(elLink) {
+    const imgContent = gElCanvas.toDataURL('image/jpeg') // image/jpeg the default format
+    elLink.href = imgContent
+}
